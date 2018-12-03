c798c72f6ba3b60b583ca157d25bbcaaf521054c
lxfriday <liu3248184446@outlook.com>
2018-12-03 17:26:58 +0800

    [modify] bash 文件添加注释

--- bash.sh
+++ bash.sh
@@ -1,12 +1,18 @@
+# 获取内容，写入文件
 cd /Users/lxfriday/works/github-trending
 npm start
+# 将形成的今日 hitory 添加到版本控制系统
 git add ./src/history.md
 git commit -m '[update] history'
 git push
+# 删除 blog 中的 githubtrending.md 原始文件
 rm /Users/lxfriday/lxfriday.github.io/source/_posts/githubtrending.md
+# 复制新文件到 source/_posts 目录
 cp ./dist/history-trending.md /Users/lxfriday/lxfriday.github.io/source/_posts/githubtrending.md
 cd /Users/lxfriday/lxfriday.github.io/
+# 把内容发布
 npm run submit
+# 将新生成的文件添加到 dev 分支
 git add ./source/_posts/githubtrending.md
 git commit -m '[update] github trending daily'
 git push
