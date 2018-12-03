e2fa5605408382af719f92271e80ed4816fc1e17
lxfriday <liu3248184446@outlook.com>
2018-12-03 17:24:35 +0800

    [modify] bash file

--- bash.sh
+++ bash.sh
@@ -1,9 +1,12 @@
 cd /Users/lxfriday/works/github-trending
 npm start
+git add ./src/history.md
+git commit -m '[update] history'
+git push
 rm /Users/lxfriday/lxfriday.github.io/source/_posts/githubtrending.md
 cp ./dist/history-trending.md /Users/lxfriday/lxfriday.github.io/source/_posts/githubtrending.md
 cd /Users/lxfriday/lxfriday.github.io/
 npm run submit
 git add ./source/_posts/githubtrending.md
-git commit -m '[update]'
+git commit -m '[update] github trending daily'
 git push
