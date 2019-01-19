d6633c8c634ed94afac1c874e90aed2429a22d70
lxfriday <liu3248184446@outlook.com>
2019-01-19 14:49:13 +0800

    [update]

--- aliyunserver.sh
+++ aliyunserver.sh
@@ -1,6 +1,6 @@
 # 获取内容，写入文件
 cd /home/uftp/github-trending
-npm start
+/usr/local/bin/npm start
 # 将形成的今日 hitory 添加到版本控制系统
 git add ./src/history.md
 git commit -m '[update] history'
@@ -11,7 +11,7 @@ rm /home/uftp/lxfriday.github.io/source/_posts/githubtrending.md
 cp ./dist/history-trending.md /home/uftp/lxfriday.github.io/source/_posts/githubtrending.md
 cd /home/uftp/lxfriday.github.io/
 # 把内容发布
-npm run submit
+/usr/local/bin/npm run submit
 # 将新生成的文件添加到 dev 分支
 git add ./source/_posts/githubtrending.md
 git commit -m '[update] github trending daily'
